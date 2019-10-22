ed36d41637f1fc6d724fe4841fdc2c801b864e3e
ThibaultGROUEIX <thibaultgroueix@orange.fr>
2019-10-22 11:37:41 +0200

    go

--- get_chamfer.py
+++ get_chamfer.py
@@ -1,13 +1,13 @@
-def get(opt):
-    if opt.dim_template == 2:
+def get(dim=3):
+    if dim == 2:
         import chamfer_2D.dist_chamfer_idx as ext
         distChamfer = ext.chamferDist()
-    elif opt.dim_template == 3:
+    elif dim == 3:
         import chamfer_3D.dist_chamfer_idx as ext
         distChamfer = ext.distChamfer
-    elif opt.dim_template == 5:
+    elif dim == 5:
         import chamfer_3D.dist_chamfer_idx as ext
         distChamfer = ext.distChamfer
     else:
-        print('missing opt.dim_template')
+        print('missing dim')
     return distChamfer
