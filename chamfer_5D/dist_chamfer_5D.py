6292d7bf680989f54def0aedd4b6b9fd9eda4c60
ThibaultGROUEIX <thibaultgroueix@orange.fr>
2019-10-22 11:54:17 +0200

    go

---
+++ chamfer_5D/dist_chamfer_5D.py
@@ -0,0 +1,58 @@
+import math
+from torch import nn
+from torch.autograd import Function
+import torch
+import sys
+from numbers import Number
+from collections import Set, Mapping, deque
+import chamfer_5D
+
+print("imported CUDA Chamfer 5D")
+
+# Chamfer's distance module @thibaultgroueix
+# GPU tensors only
+class chamfer_5DFunction(Function):
+    @staticmethod
+    def forward(ctx, xyz1, xyz2):
+        batchsize, n, _ = xyz1.size()
+        _, m, _ = xyz2.size()
+        device = xyz1.device
+
+        dist1 = torch.zeros(batchsize, n)
+        dist2 = torch.zeros(batchsize, m)
+
+        idx1 = torch.zeros(batchsize, n).type(torch.IntTensor)
+        idx2 = torch.zeros(batchsize, m).type(torch.IntTensor)
+
+        dist1 = dist1.to(device)
+        dist2 = dist2.to(device)
+        idx1 = idx1.to(device)
+        idx2 = idx2.to(device)
+        torch.cuda.set_device(device)
+
+        chamfer_5D.forward(xyz1, xyz2, dist1, dist2, idx1, idx2)
+        ctx.save_for_backward(xyz1, xyz2, idx1, idx2)
+        return dist1, dist2
+
+    @staticmethod
+    def backward(ctx, graddist1, graddist2):
+        xyz1, xyz2, idx1, idx2 = ctx.saved_tensors
+        graddist1 = graddist1.contiguous()
+        graddist2 = graddist2.contiguous()
+        device = graddist1.device
+
+        gradxyz1 = torch.zeros(xyz1.size())
+        gradxyz2 = torch.zeros(xyz2.size())
+
+        gradxyz1 = gradxyz1.to(device)
+        gradxyz2 = gradxyz2.to(device)
+        chamfer_5D.backward(xyz1, xyz2, gradxyz1, gradxyz2, graddist1, graddist2, idx1, idx2)
+        return gradxyz1, gradxyz2
+
+class chamfer_5DDist(nn.Module):
+    def __init__(self):
+        super(chamfer_5DDist, self).__init__()
+
+    def forward(self, input1, input2):
+        return chamfer_5DFunction.apply(input1, input2)
+
