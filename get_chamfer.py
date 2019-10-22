6241656a095760a3917c2a522215ab9adc6abe33
ThibaultGROUEIX <thibaultgroueix@orange.fr>
2019-10-22 11:53:48 +0200

    go

--- get_chamfer.py
+++ get_chamfer.py
@@ -1,13 +1,17 @@
+import sys
 def get(dim=3):
     if dim == 2:
-        import chamfer_2D.dist_chamfer_idx as ext
-        distChamfer = ext.chamferDist()
+        sys.path.insert(0, './extension/chamfer_2D/')
+        import dist_chamfer_idx_2D as ext_2D
+        distChamfer = ext_2D.chamfer_2DDist()
     elif dim == 3:
-        import chamfer_3D.dist_chamfer_idx as ext
-        distChamfer = ext.distChamfer
+        sys.path.insert(0, './extension/chamfer_3D/')
+        import dist_chamfer_idx_3D as ext_3D
+        distChamfer = ext_3D.chamfer_3DDist()
     elif dim == 5:
-        import chamfer_3D.dist_chamfer_idx as ext
-        distChamfer = ext.distChamfer
+        sys.path.insert(0, './extension/chamfer_5D/')
+        import dist_chamfer_idx_5D as ext_5D
+        distChamfer = ext_5D.chamfer_5DDist()
     else:
         print('missing dim')
     return distChamfer
