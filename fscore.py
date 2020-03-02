63d948a2dff72252233721c577351cb1a6cf52b8
ThibaultGROUEIX <thibaultgroueix@orange.fr>
2020-03-02 16:22:26 +0100

    Update fscore.py

--- fscore.py
+++ fscore.py
@@ -8,7 +8,7 @@ def fscore(dist1, dist2, threshold=0.001):
     :param th: float
     :return: fscore, precision, recall
     """
-
+    # NB : In this depo, dist1 and dist2 are squared pointcloud euclidean distances, so you should adapt the threshold accordingly.
     precision_1 = torch.mean((dist1 < threshold).float(), dim=1)
     precision_2 = torch.mean((dist2 < threshold).float(), dim=1)
     fscore = 2 * precision_1 * precision_2 / (precision_1 + precision_2)
