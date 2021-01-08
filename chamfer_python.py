364c03c4ec5febc1e21068ffac362eca4a8f61d9
ThibaultGROUEIX <thibaultgroueix@orange.fr>
2021-01-08 15:58:40 +0100

    Update chamfer_python.py
    Create explicit function batched pairwise dist

--- chamfer_python.py
+++ chamfer_python.py
@@ -15,6 +15,19 @@ def NN_loss(x, y, dim=0):
     return values.mean()
 
 
+def batched_pairwise_dist(a, b):
+    x, y = a.double(), b.double()
+    bs, num_points_x, points_dim = x.size()
+    bs, num_points_y, points_dim = y.size()
+
+    xx = torch.pow(x, 2).sum(2)
+    yy = torch.pow(y, 2).sum(2)
+    zz = torch.bmm(x, y.transpose(2, 1))
+    rx = xx.unsqueeze(1).expand(bs, num_points_y, num_points_x) # Diagonal elements xx
+    ry = yy.unsqueeze(1).expand(bs, num_points_x, num_points_y) # Diagonal elements yy
+    P = rx.transpose(2, 1) + ry - 2 * zz
+    return P
+
 def distChamfer(a, b):
     """
     :param a: Pointclouds Batch x nul_points x dim
@@ -26,15 +39,6 @@ def distChamfer(a, b):
     -idx of closest point on a of points from b
     Works for pointcloud of any dimension
     """
-    x, y = a.double(), b.double()
-    bs, num_points_x, points_dim = x.size()
-    bs, num_points_y, points_dim = y.size()
-
-    xx = torch.pow(x, 2).sum(2)
-    yy = torch.pow(y, 2).sum(2)
-    zz = torch.bmm(x, y.transpose(2, 1))
-    rx = xx.unsqueeze(1).expand(bs, num_points_y, num_points_x) # Diagonal elements xx
-    ry = yy.unsqueeze(1).expand(bs, num_points_x, num_points_y) # Diagonal elements yy
-    P = rx.transpose(2, 1) + ry - 2 * zz
+    P = batched_pairwise_dist(a, b)
     return torch.min(P, 2)[0].float(), torch.min(P, 1)[0].float(), torch.min(P, 2)[1].int(), torch.min(P, 1)[1].int()
 
